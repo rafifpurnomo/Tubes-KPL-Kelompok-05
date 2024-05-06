f44d3a060721a276b6ac2c453855cfc40da22daa
unknown <rafif.purnomo@gmail.com>
2024-05-06 11:04:37 +0700

    update API

---
+++ API_TUBES_KPL_KELOMPOK-05/Controllers/AkunController.cs
@@ -0,0 +1,87 @@
+﻿using API_TUBES_KPL_KELOMPOK_05.Model;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace API_TUBES_KPL_KELOMPOK_05.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AkunController : Controller
+    {
+        private static List<Akun> DataAkun = new List<Akun>();
+
+        public AkunController()
+        {
+            string jsonFilePath = "C:\\Users\\Rafif Purnomo\\OneDrive\\Documents\\Coding\\C#\\TUBES_KPL_KELOMPOK_05\\API_TUBES_KPL\\API_TUBES_KPL\\Data\\DataAkun.json";
+            string jsonData = System.IO.File.ReadAllText(jsonFilePath);
+            DataAkun = JsonConvert.DeserializeObject<List<Akun>>(jsonData);
+        }
+
+        // GET DATA ALL API
+        [HttpGet]
+        public IEnumerable<Akun> GET()
+        {
+            return DataAkun;
+        }
+
+        // GET DATA BY No_Anggota 
+        [HttpGet("{No_Anggota}")]
+        public Akun GET(string No_Anggota)
+        {
+            int id = -1;
+
+            for (int i = 0; i < DataAkun.Count; i++)
+            {
+                if (No_Anggota == DataAkun[i].No_Anggota)
+                {
+                    id = i;
+                }
+            }
+            return DataAkun[id];
+        }
+
+        // POST DATA BY No_Anggota
+        [HttpPost]
+        public void POST([FromBody] Akun newAkun)
+        {
+            int id = -1;
+            Boolean sama = false;
+
+            for (int i = 0; i < DataAkun.Count; i++)
+            {
+                if (newAkun.No_Anggota == DataAkun[i].No_Anggota)
+                {
+                    sama = true;
+                }
+            }
+
+            if (!sama)
+            {
+                DataAkun.Add(newAkun);
+                string jsonFilePath = "C:\\Users\\Rafif Purnomo\\OneDrive\\Documents\\Coding\\C#\\TUBES_KPL_KELOMPOK_05\\API_TUBES_KPL\\API_TUBES_KPL\\Data\\DataAkun.json";
+                string jsonContent = JsonConvert.SerializeObject(DataAkun);
+                System.IO.File.WriteAllText(jsonFilePath, jsonContent);
+            }
+        }
+
+        // DELETE DATA BY No_Anggota
+        [HttpDelete("{No_Anggota}")]
+        public void DELETE(String No_Anggota)
+        {
+            int id = -1;
+
+            for (int i = 0; i < DataAkun.Count; i++)
+            {
+                if (No_Anggota == DataAkun[i].No_Anggota)
+                {
+                    DataAkun.RemoveAt(i);
+                    string jsonFilePath = "C:\\Users\\Rafif Purnomo\\OneDrive\\Documents\\Coding\\C#\\TUBES_KPL_KELOMPOK_05\\API_TUBES_KPL\\API_TUBES_KPL\\Data\\DataAkun.json";
+                    string jsonContent = JsonConvert.SerializeObject(DataAkun);
+                    System.IO.File.WriteAllText(jsonFilePath, jsonContent);
+                    break;
+                }
+            }
+        }
+    }
+}
+
---
+++ API_TUBES_KPL_KELOMPOK-05/Model/DisplayBooks.cs
@@ -0,0 +1,23 @@
+﻿namespace API_TUBES_KPL_KELOMPOK_05.Model
+{
+    public class DisplayBooks
+    {
+        public string kodeBuku { get; set; }
+        public string Judul { get; set; }
+        public string Sinopsis { get; set; }
+        public string Penulis { get; set; }
+        public int TahunTerbit { get; set; }
+        public bool statusPeminjaman { get; set; }
+
+        public DisplayBooks(string kodeBuku, string judul, string sinopsis, string penulis, int tahunTerbit, bool statusPeminjaman)
+        {
+            this.kodeBuku = kodeBuku;
+            this.Judul = judul;
+            this.Sinopsis = sinopsis;
+            this.Penulis = penulis;
+            this.TahunTerbit = tahunTerbit;
+            this.statusPeminjaman = statusPeminjaman;
+            this.statusPeminjaman = statusPeminjaman;
+        }
+    }
+}
