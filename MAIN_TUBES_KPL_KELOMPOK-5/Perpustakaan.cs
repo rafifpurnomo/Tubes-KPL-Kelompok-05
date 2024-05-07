e667979de8b1c0ea8e831d9d807c17d2f2e71e3a
unknown <rafif.purnomo@gmail.com>
2024-05-07 21:59:50 +0700

    Table-Driven by Rafif

--- MAIN_TUBES_KPL_KELOMPOK-5/Perpustakaan.cs
+++ MAIN_TUBES_KPL_KELOMPOK-5/Perpustakaan.cs
@@ -10,14 +10,30 @@ namespace MAIN_TUBES_KPL_KELOMPOK_5
 {
     internal class Perpustakaan
     {
-        private List<Buku> buku;
+        private List<Buku> buku = new List<Buku>
+        {
+            new Buku("BK001", "Dasar Pemrograman JS", "Javascript", "Rafif" ,2024, 10),
+            new Buku("BK002", "Dasar Pemrograman GO", "Golang", "Belva" ,2010, 10),
+            new Buku("BK003", "Dasar Pemrograman C#", "C#", "Zidan" ,2022, 10),
+            new Buku("BK004", "Dasar Pemrograman WEB", "WEB", "Rizki" ,2009, 10),
+            new Buku("BK005", "Dasar Pemrograman C++", "C++", "Daffa" ,2021, 10),
+            new Buku("BK006", "Dasar Pemrograman Python", "Python", "Ghiyats" ,2018, 10),
+            new Buku("BK007", "Dasar Networking", "Networking", "Dana" ,2018, 10),
+        };
+
+        private Buku[] bukuArray;
+
         private List<Peminjaman> DaftarPeminjaman;
+        
         ConfigManager configManager = new ConfigManager();
+        
         public Perpustakaan()
         {
-            buku = new List<Buku>();
             DaftarPeminjaman = new List<Peminjaman>();
-
+        }
+        public void toArray()
+        {
+            bukuArray = buku.ToArray();
         }
 
         // Implementasikan Method 
@@ -62,9 +78,36 @@ namespace MAIN_TUBES_KPL_KELOMPOK_5
                 rakBuku[idx_buku].stok--;
             }
         }
-        public void tesst()
+        public void cariByJudul(string judul)
         {
-            Console.WriteLine("sdsds");
+
+            for (int i = 0; i < bukuArray.Length; i++)
+            {
+                if (judul == bukuArray[i].Judul)
+                {
+                    Console.WriteLine("Kode buku: " + bukuArray[i].kodeBuku);
+                    Console.WriteLine("Judul Buku" + bukuArray[i].Judul);
+                    Console.WriteLine("Sinopsis: " + bukuArray[i].Sinopsis);
+                    Console.WriteLine("Penulis: " + bukuArray[i].Penulis);
+                    Console.WriteLine("Tahun Terbit: " + bukuArray[i].TahunTerbit);
+                }
+            }
+        }
+
+        public int returnIndex(string judul)
+        {
+            int index = -1;
+
+            for (int i = 0; i < bukuArray.Length; i++)
+            {
+                if (judul == bukuArray[i].Judul)
+                {
+                    return index = i;
+                    break;
+                }
+            }
+
+            return index;
         }
     }
 }
